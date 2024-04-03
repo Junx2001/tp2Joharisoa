fdadf68455947573d9530810a6bda2a74bceafb9
Junx2001 <ratsirarsonj@gmail.com>
2024-04-03 21:15:21 +0300

    Etat Initial TP 2

---
+++ src/main/java/mg/itu/joharisoa/tp2joharisoa/config/ConfigJSF.java
@@ -0,0 +1,15 @@
+package mg.itu.joharisoa.tp2joharisoa.config;
+
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.annotation.FacesConfig;
+
+/**
+ * Configuration JSF
+ *
+ * @author xxxx
+ */
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+}
